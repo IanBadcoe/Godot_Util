0cbb7991c658d3fcdfdecd2ef0aae428a42ded7c
Ian Badcoe <igbadcoe@gmail.com>
2025-04-28 11:36:15 +0100

    Initial move of utils from SubD

---
+++ Util.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Godot;
+using System.Linq;
+using System;
+using System.Diagnostics;
+
+namespace Godot_Util
+{
+    public static class Util
+    {
+        internal static Vector3 Sum(this IEnumerable<Vector3> that)
+        {
+            return that.Aggregate(Vector3.Zero, (x, y) => x + y);
+        }
+
+        internal static void ForEach<T>(this IEnumerable<T> that, Action<T> action)
+        {
+            foreach(T item in that)
+            {
+                action(item);
+            }
+        }
+
+        [Conditional("DEBUG")]
+        internal static void Assert(bool b)
+        {
+            if (!b)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
