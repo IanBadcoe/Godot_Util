e3f2bd9e8d4b7511924b6a67618da34d8747f7db
Ian Badcoe <igbadcoe@gmail.com>
2025-05-21 15:08:01 +0100

    add Min methods to MathfExtensions

--- Ext/MathfExtensions.cs
+++ Ext/MathfExtensions.cs
@@ -7,7 +7,7 @@ static class MathfExtensions
     {
         int max = values.First();
 
-        foreach(int val in values.Skip(1))
+        foreach (int val in values.Skip(1))
         {
             max = Mathf.Max(max, val);
         }
@@ -19,11 +19,35 @@ static class MathfExtensions
     {
         float max = values.First();
 
-        foreach(float val in values.Skip(1))
+        foreach (float val in values.Skip(1))
         {
             max = Mathf.Max(max, val);
         }
 
         return max;
+    }
+
+    public static int Min(params int[] values)
+    {
+        int min = values.First();
+
+        foreach(int val in values.Skip(1))
+        {
+            min = Mathf.Min(min, val);
+        }
+
+        return min;
+    }
+
+    public static float Min(params float[] values)
+    {
+        float min = values.First();
+
+        foreach(float val in values.Skip(1))
+        {
+            min = Mathf.Min(min, val);
+        }
+
+        return min;
     }
 }
